27ff85037c24c1787b65c9c96c771c2248ce82aa
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2022-05-18 19:32:46 +0200

    1szy commit WSB

---
+++ src/wsb/c3/prtest/Klasa.java
@@ -0,0 +1,13 @@
+package wsb.c3.prtest;
+
+public class Klasa {
+
+    private String string = "Hello";
+
+    @Override
+    public String toString() {
+        return "Klasa{" +
+                "string='" + string + '\'' +
+                '}';
+    }
+}
---
+++ src/wsb/c3/prtest/Test.java
@@ -0,0 +1,12 @@
+package wsb.c3.prtest;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        Klasa klasa = new Klasa();
+
+        System.out.println(klasa);
+    }
+}
